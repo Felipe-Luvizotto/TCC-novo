508c54ed0a5e05837329125a39b6abd6aa69ce96
Kotaka <101439293+Felipe-Luvizotto@users.noreply.github.com>
2025-10-09 10:02:20 -0300

    TCC FINALIZADO

--- back-end/treinamento_acelerado.py
+++ back-end/treinamento_acelerado.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:61f0c8002310fdda35a7da8c4463795a5f928ce527502c25309c3c5fca8d9149
-size 5139
+oid sha256:784bd910ddb480c390d79e0ec4356e5c57037ab4dc4787ebfdcf62c6ad27b164
+size 5460
